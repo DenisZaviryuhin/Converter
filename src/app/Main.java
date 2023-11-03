b2ae9d136d49c0158f1ed99fd31d1525848dadb1
DenisZaviryuhin <denzava30@gmail.com>
2023-11-03 21:29:47 +0200

    Add reverse operation

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,19 +10,17 @@ public class Main {
     static final double CONST = 1.609;
 
     public static void main(String[] args) {
-        System.out.println("Converter from miles to kilometers");
-        System.out.print("In kilometers : " + doConvertKilometers());
+        System.out.println("Converter from kilometers to miles ");
+        System.out.print( "In miles : " + doConvertMiles());
 
     }
-
-    private static String doConvertKilometers() {
+    private static String doConvertMiles(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter miles :");
+        System.out.print("Enter Kilometers :");
         kms = scanner.nextDouble();
-        convertValue = kms * CONST;
+        convertValue = kms / CONST;
         return roundValue(convertValue);
     }
-
     public static String roundValue(double value) {
         return new DecimalFormat("#.000").format(value);
     }
